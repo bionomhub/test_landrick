49ed8ed43647ae8c906b71cddd7c8fa566dcba87
bionomhub <bionomhub@yandex.ru>
2022-01-25 20:40:37 +0300

    firebase firebaseui

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase/compat/app';
+import * as firebaseui from 'firebaseui'
+import 'firebaseui/dist/firebaseui.css'
 
 Vue.config.productionTip = false
 
